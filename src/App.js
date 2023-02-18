5d339eee72fb6b7f7cd1def8be28dd5380d8c03f
barbarajg <basjgamboa@gmail.com>
2023-02-18 18:40:54 +0100

    Task component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import logo from './images/header.jpg';
+import Task from './components/Task.js'
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       </div>
       <div className='main-task-list-container'>
         <h1>My Tasks</h1>
-
+        <Task text='Finish this' />
       </div>
     </div>
   );
---
+++ src/components/Task.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/Task.css'
+
+const Task = ({text}) => {
+
+    return (
+        <div className='task-container'>
+            <div className='task-text'>
+                {text}
+            </div>
+            <div className='task-icon'>
+                Delete
+            </div>
+        </div>
+    );
+}
+
+export default Task;
