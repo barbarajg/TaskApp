b00e924c22c69db98e3ca6047eb7265f952c44ee
barbarajg <basjgamboa@gmail.com>
2023-02-18 18:17:50 +0100

    Added logo and App styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import logo from './images/header.jpg';
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="task-app">
+      <div className='logo-container'>
+      <img
+          src={logo}
+          className='logo-image'
+          alt='Logo' 
+      />
+      </div>
+      <div className='main-task-list-container'>
+        <h1>My Tasks</h1>
+
+      </div>
     </div>
   );
 }
